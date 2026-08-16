a939f961ce20224feb6e91e9290c03d7509663e7
ahmbayo <aeeytb@gmail.com>
2026-08-16 19:15:24 +0300

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.abspath(_file_)))
 import uvicorn
 from starlette.applications import Starlette
 from starlette.responses import HTMLResponse, JSONResponse
@@ -46,6 +46,6 @@ app = Starlette(
     ]
 )
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
